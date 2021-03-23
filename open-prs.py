3fe026a7aa88b003d43dea93d46d5bf04208cf94
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2021-03-23 22:14:48 +0100

    Learning to work with dicts

--- open-prs.py
+++ open-prs.py
@@ -1,18 +1,21 @@
 import requests
 import sys
+import json
 
-query = """{ "query" : "query {
+
+def get_query(end_cursor=""):
+    graph_query = """{ "query" : "query {
       rateLimit {
         limit
         cost
         remaining
         resetAt
       }
-      organization (login: \\"ORGANIZATION\\") {
+      organization (login: \\"$ORGANIZATION\\") {
         name
-        team (slug: \\"TEAM\\") {
+        team (slug: \\"$TEAM\\") {
           name
-          repositories (first: 100 ) {
+          repositories (first: 100 $AFTER) {
             edges {
               node {
                 name
@@ -25,7 +28,6 @@ query = """{ "query" : "query {
                         login
                       }
                       createdAt
-                      reviewDecision
                     }
                   }
                 }
@@ -40,13 +42,16 @@ query = """{ "query" : "query {
       }
     }"
     }"""
+    if end_cursor != "":
+        graph_query = graph_query.replace("$AFTER", "after: " + end_cursor)
+    else:
+        graph_query = graph_query.replace("$AFTER", "")
+    return graph_query.replace('\n', '')
 
-organization = sys.argv[1]
-team = sys.argv[2]
 
-query = query.replace('\n', '')
-query = query.replace("ORGANIZATION", organization)
-query = query.replace("TEAM", team)
+query = get_query()\
+    .replace("$ORGANIZATION", sys.argv[1])\
+    .replace("$TEAM", sys.argv[2])
 
 url = "https://api.github.com/graphql"
 
@@ -54,6 +59,19 @@ token = sys.stdin.read()
 headers = {"Authorization": "Bearer " + token,
            "Content-Type": "application/json"}
 
-print(query)
 response = requests.post(url, data=query, headers=headers)
-print(response.text)
+jdict = json.loads(response.text)
+repos = jdict["data"]["organization"]["team"]["repositories"]["edges"]
+
+pr_list = []
+for repo in repos:
+    name = repo["node"]["name"]
+    prs = repo["node"]["pullRequests"]["edges"]
+    for pr_node in prs:
+        pr = pr_node["node"]
+        pr["name"] = name
+        pr["user"] = pr["author"]["login"]
+        pr.pop("author")
+        pr_list.append(pr)
+sorted_list = sorted(pr_list, key=lambda k: k["user"])
+print(sorted_list)
--- open-prs.sh
+++ open-prs.sh
@@ -1,4 +1,4 @@
-ORG="ORG"
-TEAM="TEAM"
+ORG="-"
+TEAM="-"
 
 secret-tool lookup kejne open-prs | python3 open-prs.py $ORG $TEAM
