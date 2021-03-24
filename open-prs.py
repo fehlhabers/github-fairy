681e92d58e33d24f4bafc36add743476c9508845
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2021-03-24 21:58:01 +0100

    Making it ok to face some public

--- open-prs.py
+++ open-prs.py
@@ -2,6 +2,12 @@ import requests
 import sys
 import json
 
+CREATED_WIDTH = 10
+REPO_WIDTH = 20
+AUTHOR_WIDTH = 15
+URL_WIDTH = 100
+TITLE_WIDTH = 53
+
 
 def get_query(end_cursor=""):
     graph_query = """{ "query" : "query {
@@ -49,29 +55,58 @@ def get_query(end_cursor=""):
     return graph_query.replace('\n', '')
 
 
-query = get_query()\
-    .replace("$ORGANIZATION", sys.argv[1])\
+def github_request(complete_query):
+    url = "https://api.github.com/graphql"
+    token = sys.stdin.read()
+    headers = {"Authorization": "Bearer " + token,
+               "Content-Type": "application/json"}
+    return requests.post(url, data=complete_query, headers=headers)
+
+
+def convert_response(repos):
+    converted_pr_list = []
+    for repo in repos:
+        name = repo["node"]["name"]
+        prs = repo["node"]["pullRequests"]["edges"]
+        for pr_node in prs:
+            pr = pr_node["node"]
+            pr["name"] = name
+            pr["user"] = pr["author"]["login"]
+            pr.pop("author")
+            converted_pr_list.append(pr)
+    return converted_pr_list
+
+
+def print_header():
+    print("PR".ljust(TITLE_WIDTH - 2) +
+          "| URL".ljust(URL_WIDTH) +
+          "| Repository".ljust(REPO_WIDTH) +
+          "| Author".ljust(AUTHOR_WIDTH) +
+          "| Created".ljust(CREATED_WIDTH))
+    print("=".ljust(200, '='))
+
+
+def print_line(pr):
+    line = pr["title"][0:TITLE_WIDTH - 2].ljust(TITLE_WIDTH) + \
+           pr["url"][0:URL_WIDTH - 2].ljust(URL_WIDTH) + \
+           pr["name"][0:REPO_WIDTH - 2].ljust(REPO_WIDTH) + \
+           pr["user"][0:AUTHOR_WIDTH - 2].ljust(AUTHOR_WIDTH) + \
+           pr["createdAt"][0:CREATED_WIDTH]
+    print(line)
+
+
+query = get_query() \
+    .replace("$ORGANIZATION", sys.argv[1]) \
     .replace("$TEAM", sys.argv[2])
+sort_by = sys.argv[3]
 
-url = "https://api.github.com/graphql"
+response = github_request(query)
 
-token = sys.stdin.read()
-headers = {"Authorization": "Bearer " + token,
-           "Content-Type": "application/json"}
+repos = json.loads(response.text)["data"]["organization"]["team"]["repositories"]["edges"]
 
-response = requests.post(url, data=query, headers=headers)
-jdict = json.loads(response.text)
-repos = jdict["data"]["organization"]["team"]["repositories"]["edges"]
+pr_list = convert_response(repos)
+sorted_list = sorted(pr_list, key=lambda k: k[sort_by])
+print_header()
 
-pr_list = []
-for repo in repos:
-    name = repo["node"]["name"]
-    prs = repo["node"]["pullRequests"]["edges"]
-    for pr_node in prs:
-        pr = pr_node["node"]
-        pr["name"] = name
-        pr["user"] = pr["author"]["login"]
-        pr.pop("author")
-        pr_list.append(pr)
-sorted_list = sorted(pr_list, key=lambda k: k["user"])
-print(sorted_list)
+for pr in sorted_list:
+    print_line(pr)
--- open-prs.sh
+++ open-prs.sh
@@ -1,4 +1,21 @@
 ORG="-"
 TEAM="-"
 
-secret-tool lookup kejne open-prs | python3 open-prs.py $ORG $TEAM
+case $1 in
+"date")
+  SORT_BY="createdAt"
+  ;;
+"repo")
+  SORT_BY="name"
+  ;;
+"user")
+  SORT_BY="user"
+  ;;
+*)
+  echo "Usage: ./open-prs.sh [date repo user]"
+  echo "Choose how to sort PRs"
+  exit 2;
+  ;;
+esac
+
+secret-tool lookup fehlhabers open-prs | python3 open-prs.py $ORG $TEAM $SORT_BY
